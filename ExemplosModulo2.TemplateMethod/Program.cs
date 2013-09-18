5a0effb6e4280d22bcd7271372d46c1a964017e6
fabiomargarito <fabiomargarito@gmail.com>
2013-09-17 23:39:16 -0300

    Versão 1.0.0.0

---
+++ ExemplosModulo2.TemplateMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExemplosModulo2.TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dalCliente = new Templa
+
+
+        }
+    }
+}
